5deb15a87992cc3bbd72b054a2fdd426ad9d678d
g-drol <guyd1491@hotmail.fr>
2017-01-07 13:50:50 -0500

    SecondaryController updated

--- Assets/Scripts/MainPController.cs
+++ Assets/Scripts/MainPController.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class MainPController : MonoBehaviour {
 
+	private Animator animator;
+	private InputDevice inputDevice;
+
+	[SerializeField]
+	private int playerNum;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
+		inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
