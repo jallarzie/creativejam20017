8a2c41bbfa7ed282acf5e656aabf3106a3e664c1
g-drol <guyd1491@hotmail.fr>
2017-01-07 12:36:24 -0500

    Controller Updates & Animations
    Animations and Controller Updates

---
+++ Assets/Scripts/AssignationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class InputController : MonoBehaviour {
+
+	void Start(){
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		//Check list devices
+		//Si A appuyez assign au prochain
+		//Needs Array
+	}
+}
--- Assets/Scripts/SecondaryPController.cs
+++ Assets/Scripts/SecondaryPController.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class SecondaryPController : MonoBehaviour {
 
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
+		//jumping = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
 	}
 }
