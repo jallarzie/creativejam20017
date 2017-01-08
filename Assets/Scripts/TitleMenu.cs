e911754fb5f67474ff46e39e5fa1034b4dc8a663
g-drol <guyd1491@hotmail.fr>
2017-01-07 22:18:11 -0500

    Title Animation fix

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using InControl;
+
+public class TitleMenu : MonoBehaviour {
+
+	[SerializeField]
+	private Animator canvasAnimator;
+	[SerializeField]
+	private GameObject instructions;
+
+	public void LoadScene(string loadedScene){
+		SceneManager.LoadScene (loadedScene, LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (InputManager.ActiveDevice != null) {
+			if (InputManager.ActiveDevice.GetControl (InputControlType.Start)) {
+				Application.Quit ();
+			}
+			if (InputManager.ActiveDevice.AnyButton.WasPressed) {
+				if (!instructions.activeSelf) {
+					canvasAnimator.Stop ();
+					instructions.SetActive (true);
+				} else if (instructions.activeSelf) {
+					LoadScene ("scene_main");			
+				}
+			}
+		}
+	}
+}
