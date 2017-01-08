ee95a4be6b1f0514dc34551aab42e1bd59c019fa
g-drol <guyd1491@hotmail.fr>
2017-01-07 22:15:49 -0500

    MenuScenes and Off_Balance Animations

---
+++ Assets/Scripts/StoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using InControl;
+
+public class MainMenu : MonoBehaviour {
+	
+	[SerializeField]
+	private Animator canvasAnimator;
+
+	public void LoadScene(string loadedScene){
+		SceneManager.LoadScene (loadedScene, LoadSceneMode.Single);
+    }
+
+    public void Update()
+    {
+		if (Time.timeSinceLevelLoad < 12f) {
+			LoadScene ("scene_title");
+		}
+		if (InputManager.ActiveDevice != null) {
+			if (InputManager.ActiveDevice.GetControl (InputControlType.Start)) {
+				Application.Quit ();
+			}
+		}
+
+    }
+}
